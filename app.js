fe51c7b9f24c2fa30a0502a279cb7fe2be894b2c
Eduard <eduard.duarte@hotmail.com>
2020-01-30 00:26:38 -0500

    done crud v0.1

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const db = require ('./queries')
 const app = express()
 const port = 80
 
+var cors = require ('cors');
+app.use(cors({
+    origin:['http://localhost:4200','http://127.0.0.1:4200'],
+    credentials:true
+}));
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
+
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -12,7 +28,6 @@ app.use(
   })
 )
 
-console.log("hola...")
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
@@ -22,16 +37,19 @@ app.get('/', (request, response) => {
 app.get('/users', db.getUsers);
 
 //Crear un USUARIO nota: no permite duplicados de correo
-app.post('/user', db.postUser);
+app.post('/createUser', db.postUser);
 
 //Validar login con correo y contraseña
-app.get('/login', db.getLogin);
+app.post('/login', db.getLogin);
 
-//Crear un evento app.post('/postEvent')
+//Crear un evento
+app.post('/creatEvent', db.postEvent);
 
-//Eliminar un evento app.post('/deleteEvent')
- 
-//Actualizar un evento app.post('/putEvent')
+//Eliminar un evento
+app.delete('/deleteEvent', db.deleteEvent);
+
+//Actualizar un evento
+app.put('/updatevent', db.putEvent);
 
 
 
--- queries.js
+++ queries.js
@@ -12,13 +12,16 @@ const pool = new Pool({
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM public."Users" ORDER BY id_user ASC', (error, results) => {
     if (error) {
-      throw error
+      console.log(error);
+      return;
     }
     response.status(200).json(results.rows)
   })
 }
 
 const getLogin = (request, response) => {
+  console.log(request.body);
+  console.log(request);
   // const id = parseInt(request.params.id)
   const {
     email,
@@ -28,7 +31,8 @@ const getLogin = (request, response) => {
 
   pool.query('SELECT * FROM public."Users" WHERE email = $1 AND contrasena = $2', [email, contrasenia], (error, results) => {
     if (error) {
-      throw error
+      console.log(error);
+      return;
     }
     let resp = new res.resp(results.rowCount == '0' ? 'ER' : 'OK', results.command, results.rows[0]);
     response.status(200).json(resp);
@@ -41,7 +45,8 @@ const getUserById = (request, response) => {
 
   pool.query('SELECT * FROM public."Users" WHERE id_user = $1', [id], (error, results) => {
     if (error) {
-      throw error
+      console.log(error);
+      return;
     }
     response.status(200).json(results.rows)
   })
@@ -53,82 +58,118 @@ const postUser = (request, response) => {
     apellidos,
     email,
     contrasenia
-  } = request.body
+  } = request.body;
   pool.query('INSERT INTO public."Users"(nombres, apellidos, email, contrasena)	VALUES ($1, $2, $3, $4)',
     [nombres, apellidos, email, contrasenia], (error, results) => {
       if (error) {
         // console.log("results", results);
         // console.log("error: ", error);
         let resp = new res.resp(error.severity, error.detail, error);
-        response.status(403).send(resp)
-        // throw error;
+        response.status(403).send(resp);
+        console.log(error);
+        return;
       }
       if (results) {
         let resp = new res.resp('OK', results.command, `SE CREO EL USUARIO ${email}`);
-        response.status(201).send(resp)
+        response.status(201).send(resp);
       }
     })
-}
+};
 
 
 const postEvent = (request, response) => {
   const {
-    nombres,
-    apellidos,
-    email,
-    contrasenia
-  } = request.body
-  pool.query('INSERT INTO public."Users"(nombres, apellidos, email, contrasena)	VALUES ($1, $2, $3, $4)',
-    [nombres, apellidos, email, contrasenia], (error, results) => {
+    id_user,
+    nombre,
+    lugar,
+    direccion,
+    date_inicio,
+    date_fin,
+    tipo_evento,
+    id_categoria
+  } = request.body;
+  pool.query('INSERT INTO public."eventos"(id_user, nombre, lugar, direccion, date_inicio, date_fin, tipo_evento, id_categoria)	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+    [id_user, nombre, lugar, direccion, date_inicio, date_fin, tipo_evento, id_categoria], (error, results) => {
       if (error) {
         // console.log("results", results);
         // console.log("error: ", error);
         let resp = new res.resp(error.severity, error.detail, error);
         response.status(403).send(resp)
-        // throw error;
+        console.log(error);
+        return;
       }
       if (results) {
-        let resp = new res.resp('OK', results.command, `SE CREO EL USUARIO ${email}`);
+        let resp = new res.resp('OK', results.command, `SE CREO EL EVENTO ${nombre}`);
         response.status(201).send(resp)
       }
     })
-}
+};
 
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
+
+const putEvent = (request, response) => {
+
   const {
-    name,
-    email
+    id,
+    id_user,
+    nombre,
+    lugar,
+    direccion,
+    date_inicio,
+    date_fin,
+    id_categoria,
+    tipo_evento
   } = request.body
+  console.log(id, id_user, nombre, lugar, direccion, date_inicio, date_fin, id_categoria, tipo_evento);
 
   pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id_user = $3',
-    [name, email, id],
+    'UPDATE public."eventos" SET nombre=$3, lugar=$4, direccion=$5, date_inicio=$6, date_fin=$7, id_categoria=$8, tipo_evento=$9 WHERE id=$1 and id_user = $2',
+    [id, id_user, nombre, lugar, direccion, date_inicio, date_fin, id_categoria, tipo_evento],
     (error, results) => {
       if (error) {
-        throw error
+        let resp = new res.resp(error.severity, error.detail, error);
+        response.status(403).send(resp)
+        console.log(error);
+        return;
       }
-      response.status(200).send(`User modified with ID: ${id}`)
+      console.log(results);
+      if (results.rowCount === 1)
+        var resp = new res.resp('OK', results.command, `Evento UpDate with ID: ${id}`);
+      else if (results.rowCount === 0)
+        var resp = new res.resp('ER', 'NO_FOUND', `Evento con ID: ${id} y user ${id_user} no hizo UpDate`);
+      else
+        var resp = new res.resp('OK', results.command, results);
+      response.status(200).send(resp);
     }
   )
 }
 
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
+const deleteEvent = (request, response) => {
+  const id = parseInt(request.body.id);
+  const id_user = parseInt(request.body.id_user);
 
-  pool.query('DELETE FROM public."Users" WHERE id_user = $1', [id], (error, results) => {
+  pool.query('DELETE FROM public."eventos" WHERE id = $1 and id_user = $2', [id, id_user], (error, results) => {
     if (error) {
-      throw error
+      let resp = new res.resp(error.severity, error.detail, error);
+      response.status(403).send(resp)
+      console.log(error);
+      return;
     }
-    response.status(200).send(`User deleted with ID: ${id}`)
+    if (results.rowCount === 1)
+      var resp = new res.resp('OK', results.command, `Evento deleted with ID: ${id}`);
+    else if (results.rowCount === 0)
+      var resp = new res.resp('ER', 'NO_FOUND', `Evento con ID: ${id} y user ${id_user} no fue Eliminado`);
+    else
+      var resp = new res.resp('OK', results.command, results);
+    response.status(200).send(resp)
   })
 }
 
 module.exports = {
   getUsers,
   postUser, //createUser
+  postEvent, //creatEvent
+  deleteEvent, //deleteEvent
+  putEvent, //upDateEvent
   getLogin,
   getUserById,
-  updateUser,
-  deleteUser,
 }
