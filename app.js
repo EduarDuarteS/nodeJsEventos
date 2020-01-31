fe7eb5671a9840ac9005c9b99d57bd8fc7d112c6
Eduard <eduard.duarte@hotmail.com>
2020-01-30 23:26:11 -0500

    cambio de puertos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const db = require ('./queries')
 const app = express()
-const port = 80
+const port = 8081;
 
 var cors = require ('cors');
 app.use(cors({
